7ffd8c7a6ced07bb709164e3b970883b85927fea
Alexander Dyankov <alexander.dyankov@gmail.com>
2025-04-22 02:44:46 +0300

    commitche6

--- DiscordBotAccManager/src/main/java/com/alexander/persistence/model/entities/UserAccEntity.java
+++ DiscordBotAccManager/src/main/java/com/alexander/persistence/model/entities/UserAccEntity.java
@@ -14,12 +14,9 @@ public class UserAccEntity extends BaseEntity {
 //    private String passwordHash;
 
     @OneToOne
-    @JoinColumn(nullable = true, unique = true, name = "apiKeyId", referencedColumnName = "id")
+    @JoinColumn(nullable = true, unique = true, name = "apiKey", referencedColumnName = "id")
     private ApiKeyEntity apiKey;
 
-    @OneToOne
-    @JoinColumn(nullable = true, unique = true, name = "securityTokenId", referencedColumnName = "id")
-    private SecurityTokenEntity securityToken;
 
 
 }
--- DiscordBotAccManager/src/main/java/com/alexander/persistence/repository/SecurityTokenRepository.java
+++
@@ -1,6 +0,0 @@
-package com.alexander.persistence.repository;
-
-import com.alexander.persistence.model.entities.SecurityTokenEntity;
-
-public interface SecurityTokenRepository extends MyRepository<SecurityTokenEntity>{
-}
